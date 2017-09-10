0d34fcfb6805f8a5334792fff12d8da5423f88d7
xdk78 <xdk78@interia.pl>
2017-09-10 11:20:02 +0200

    Add basic Timetable for one day

--- src/renderer/components/Timetable.vue
+++ src/renderer/components/Timetable.vue
@@ -1,30 +1,42 @@
 <template>
-    <v-layout justify-center align-center row wrap>
-        <v-flex xs12 sm8 md6>
-            <ul v-for="day in timetables.Timetable">
-              <li>{{day}}</li>
-              </ul>
-        </v-flex>
-    </v-layout>
+    <v-container fluid style="min-height: 0;" grid-list-lg>
+        <div>
+            <h3 class="text-sm-center">{{date}}</h3>
+        </div>
+        <v-layout justify-center align-center row wrap>
+            <v-flex xs12 sm8 v-for="days in timetables">
+                <v-card>
+                    <v-card-title primary-title v-for="day in days">
+                        <div>
+                            <h3 class="headline mb-0">{{day.LessonNo}}</h3>
+                            <div>{{day.Subject.Name}}</div>
+                        </div>
+                    </v-card-title>
+                </v-card>
+            </v-flex>
+        </v-layout>
+    </v-container>
 </template>
 
 <script>
   /* eslint-disable indent */
   import api from '../api'
+  // import _ from 'lodash'
   export default {
     name: 'timetable',
     data () {
       return {
-        timetables: []
+        timetables: Object,
+        date: '2017-09-07'
       }
     },
     mounted () {
       api.request('/Timetables').then((data) => {
         console.log(data)
-        this.timetables = data
+        this.timetables = data.Timetable[this.date]
         }).catch((error) => {
           console.log(error)
-          })
+      })
     },
     methods: {
     }
