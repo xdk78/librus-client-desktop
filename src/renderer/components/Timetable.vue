3db9cb59769a3a4d0e20063dc78dc6a200a6cfa0
xdk78 <xdk78@interia.pl>
2017-09-10 13:14:54 +0200

    Add Timetable almost done

--- src/renderer/components/Timetable.vue
+++ src/renderer/components/Timetable.vue
@@ -1,19 +1,20 @@
 <template>
     <v-container fluid style="min-height: 0;" grid-list-lg>
-        <div>
-            <h3 class="text-sm-center">{{date}}</h3>
-        </div>
-        <v-layout justify-center align-center row wrap>
-            <v-flex xs12 sm8 v-for="days in timetables">
+        <v-layout justify-center align-center row wrap v-for="(days, key, index) in timetables">
+            <v-flex xs12 sm8>
+                <h6 class="primary--text text-sm-center">{{date[index]}}</h6>
+            </v-flex>
+            <v-flex xs12 sm8 v-for="oneday in days">
                 <v-card>
-                    <v-card-title primary-title v-for="day in days">
+                    <v-card-title primary-title v-for="da in oneday">
                         <div>
-                            <h3 class="headline mb-0">{{day.LessonNo}}</h3>
-                            <div>{{day.Subject.Name}}</div>
+                            <h3 class="headline mb-0"> {{da.LessonNo}}</h3>
+                            <div>{{da.Subject.Name}}</div>
                         </div>
                     </v-card-title>
                 </v-card>
             </v-flex>
+            </div>
         </v-layout>
     </v-container>
 </template>
@@ -27,13 +28,20 @@
     data () {
       return {
         timetables: Object,
-        date: '2017-09-07'
+        date: []
       }
     },
     mounted () {
       api.request('/Timetables').then((data) => {
         console.log(data)
-        this.timetables = data.Timetable[this.date]
+        this.timetables = data.Timetable
+        let object = data.Timetable
+      for (let key in object) {
+        if (object.hasOwnProperty(key)) {
+          console.log(key)
+          this.date.push(key)
+        }
+      }
         }).catch((error) => {
           console.log(error)
       })
